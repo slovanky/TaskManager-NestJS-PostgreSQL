8734593f7105a0df580faab3940a7a5ffc54502f
Peter Slovanky <slovanky@gmail.com>
2025-03-12 00:26:07 +0200

    handling updating task status

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -30,6 +30,14 @@ export class TasksService {
     return task;
   }
 
+  updateTaskStatus(id: string, status: TaskStatus): Task | undefined {
+    const task = this.getTaskById(id);
+    if (task) {
+      task.status = status;
+    }
+    return task;
+  }
+
   deleteTaskById(id: string): void {
     this.tasks = this.tasks.filter((task) => task.id !== id);
   }
